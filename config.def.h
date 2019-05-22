b3d8d0864a7dc31dc758ea0923cce4dbf833f064
Alexander Virgona <alexander.virgona@uts.edu.au>
2019-05-22 16:36:26 +1000

    Changed font to Roboto Mono
    Source code pro is too wide

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro for Powerline:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Roboto Mono for Powerline:size=10";
 static int borderpx = 2;
 
 /*
